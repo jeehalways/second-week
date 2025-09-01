5c0a5c226ce090694c3dc6d6aac1531fc6c86361
jeehalways <jeeh.always@hotmail.com>
2025-09-01 12:07:01 +0200

    added exercise 8 ( success-error) to the folder

---
+++ src/a08-success-error.ts
@@ -0,0 +1,16 @@
+const randomOperation = (
+  successCallback: (msg: string) => void,
+  errorCallback: (err: string) => void
+): void => {
+  if (Math.random() > 0.5) {
+    successCallback("Operation succeeded!");
+  } else {
+    errorCallback("Operation failed!");
+  }
+};
+
+// Example
+randomOperation(
+  (msg) => console.log("✅", msg),
+  (err) => console.log("❌", err)
+);
